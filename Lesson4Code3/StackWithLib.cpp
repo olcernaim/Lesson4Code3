e5b4ca6bbd3d836f80667bd9e1c4593f5d83773a
naimolcer <naimolcer@gmail.com>
2023-03-16 16:33:57 +0300

    Stack Library Çalışması

---
+++ Lesson4Code3/StackWithLib.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<stack>
+
+using namespace std;
+
+void DisplayList(stack<int> st)
+{
+	while (!st.empty())
+	{
+		cout << st.top()<<",";
+		st.pop();
+	}
+
+	cout << endl;
+}
+
+int main()
+{
+	stack<int> st;
+	st.push(5);
+	st.push(4);
+	st.push(3);
+	st.push(2);
+	st.push(1);
+
+	DisplayList(st);
+
+	cout << "Peek value is : " << st.top() << endl;
+	st.pop();
+	cout << "New peek value is : " << st.top() << endl;
+	cout << "Stack count is : " << st.size() << endl;
+	cout << "Is Stack empty : " << st.empty() << endl;
+
+
+	//ListeyiGoster();
+}
